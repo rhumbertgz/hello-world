74e5561ebbacf52a1bfc12c761da1b623bdc2483
Rodriguez Avila Humberto <humberto.rodriguez_avila@nokia-bell-labs.com>
2023-07-12 15:23:05 +0200

    add provider

---
+++ src/language/hello-world-completion.ts
@@ -0,0 +1,42 @@
+import { DefaultCompletionProvider, LangiumDocument } from 'langium';
+import {
+  CompletionList,
+  CompletionItemKind,
+  CompletionParams,
+  CompletionItem
+} from 'vscode-languageserver';
+
+type Suggestions = Promise<CompletionList | undefined>;
+
+
+export class CustomCompletionProvider extends DefaultCompletionProvider {
+  override async getCompletion(doc: LangiumDocument, params: CompletionParams): Suggestions {
+    const list = await super.getCompletion(doc, params);
+    console.log('getCompletion: ');
+    if (list !== undefined) {
+      const snippets: CompletionItem[]  = [
+        {
+          label: 'person',
+          kind: CompletionItemKind.Snippet,
+          insertText: 'person ${1:name}',
+          documentation: 'Define a new person',
+        },
+        {
+          label: 'hello',
+          kind: CompletionItemKind.Snippet,
+          insertText: 'hello ${1:person}',
+          documentation: 'Define a new greeting',
+        }
+      ]; 
+
+      list.items.push(...snippets);
+      console.log('list: ', list);
+    } else {
+      console.log('list undifined')
+    }
+
+    return list;
+  }
+}
+
+
